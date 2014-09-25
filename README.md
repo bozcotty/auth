a6c1517c4aa106eb4300e657a9bf00b832ce09b0
Scott Hale <shalemont@gmail.com>
2014-09-24 23:15:03 -0700

    all the things

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rails Authorization from Scratch
+
+## Description
+This exercise follows along Railscast #250 to set up a quick and dirty authorization sign-in-out-up/log in-out process in a Rails app. I did this using Rails 3.2.13. The bcrypt-ruby gem is integral to the success of this procedure as it accesses ActiveModel's has_secure_password method
+
+## Authors
+
+* Scott Hale (bozcotty)
+
+## Collaborators and Sources
+
+* Railscast #250
+* checked my results against https://github.com/codefellows/auth
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '69d1f0a03f7674999d6beed3c2d5b8e57283f6e1bf4d0432f6fbaaae7f741c818ea9dd6c82569ef81cfc8902a6aa50298e71e0feea6d2fffc49adb2fddbb3a28'
